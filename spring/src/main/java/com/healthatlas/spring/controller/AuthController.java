abfa4a459c440fc3a56f846231ef95d75881928e
Prisha Garg <prishag.ug23.ma@nitp.ac.in>
2025-10-13 09:47:51 +0530

    .

--- spring/src/main/java/com/healthatlas/spring/controller/AuthController.java
+++ spring/src/main/java/com/healthatlas/spring/controller/AuthController.java
@@ -1,7 +1,7 @@
 package com.healthatlas.spring.controller;
 
 import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +14,7 @@ import com.healthatlas.spring.service.AuthService;
 
 @RestController
 @RequestMapping("/api/auth")
-// @CrossOrigin(origins = "https://health-atlas.vercel.app") // React frontend
+@CrossOrigin(origins = "https://health-atlas.vercel.app") // React frontend
 public class AuthController {
     private final AuthService authService;
 
