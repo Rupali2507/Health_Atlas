a86e4ab7b3700a260c985f35670c6073711bcb58
Prisha Garg <prishag.ug23.ma@nitp.ac.in>
2025-10-13 09:35:43 +0530

    changes

--- spring/src/main/java/com/healthatlas/spring/dto/SignUpRequest.java
+++ spring/src/main/java/com/healthatlas/spring/dto/SignUpRequest.java
@@ -1,8 +1,12 @@
 package com.healthatlas.spring.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class SignUpRequest {
     private String name;
     private String email;
