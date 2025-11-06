c97e58d60effe8e0359be33bc9d96680551821f1
Rupali2507 <rupalik.ug23.ma@nitp.ac.in>
2025-11-06 12:23:43 +0530

    fixed

--- frontend/src/Components/ProtectedRoute.jsx
+++ frontend/src/Components/ProtectedRoute.jsx
@@ -4,7 +4,6 @@ import { Navigate } from "react-router-dom";
 const ProtectedRoute = ({ children }) => {
   const token = localStorage.getItem("token");
 
-  // make sure no invalid token sneaks through
   if (!token || token === "undefined" || token === "null") {
     return <Navigate to="/signin" replace />;
   }
