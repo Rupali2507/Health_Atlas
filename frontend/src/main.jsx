3bf8e535da6283b4181fe218ddf416be4fd87a7f
Rupali2507 <rupalik.ug23.ma@nitp.ac.in>
2025-11-08 11:18:56 +0530

    added loader

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { HealthProvider } from "./Context/HealthContext.jsx";
+import { LoaderProvider } from "./Context/LoaderContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <HealthProvider>
-      <App />
-    </HealthProvider>
-  </BrowserRouter>
+  <LoaderProvider>
+    <BrowserRouter>
+      <HealthProvider>
+        <App />
+      </HealthProvider>
+    </BrowserRouter>
+  </LoaderProvider>
 );
