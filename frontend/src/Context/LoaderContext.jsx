c2895be53ff66d410a73688db6c9540f6a0aa258
Rupali2507 <rupalik.ug23.ma@nitp.ac.in>
2025-11-08 11:18:56 +0530

    added loader

---
+++ frontend/src/Context/LoaderContext.jsx
@@ -0,0 +1,26 @@
+// src/Context/LoaderContext.jsx
+import React, { createContext, useContext, useState } from "react";
+
+const LoaderContext = createContext();
+
+export const LoaderProvider = ({ children }) => {
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState("Loading...");
+
+  const showLoader = (msg = "Loading...") => {
+    setMessage(msg);
+    setLoading(true);
+  };
+
+  const hideLoader = () => setLoading(false);
+
+  return (
+    <LoaderContext.Provider
+      value={{ loading, message, showLoader, hideLoader }}
+    >
+      {children}
+    </LoaderContext.Provider>
+  );
+};
+
+export const useLoader = () => useContext(LoaderContext);
